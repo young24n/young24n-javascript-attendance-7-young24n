dca965ea463df2a84f9d32cfbd20612d74f51939
young24n <won8560@gmail.com>
2024-12-14 15:22:37 +0900

    feat: 파일을 읽어 학생 객체를 만드는 기능 추가
    - 파일을 읽고 정제하여 학생 객체를 생성 하는 기능 추가

---
+++ src/ReadFile.js
@@ -0,0 +1,25 @@
+import fs from 'fs';
+import Student from './Student.js';
+
+class ReadFile {
+    readAttendances() {
+        const DATA = fs.readFileSync('public/attendances.csv', 'utf-8');
+
+        return DATA;
+    }
+
+    dataCleansing() {
+        const DATA = this.readAttendances();
+        let ROW_DATA = DATA.split("\r\n");
+        ROW_DATA.shift()
+        ROW_DATA.pop()
+
+        const STUDENTS = ROW_DATA.map(line => new Student(line.split(',')))
+        STUDENTS.forEach(student => student.readStudentAttendance());
+
+        return STUDENTS
+    }
+
+}
+
+export default ReadFile;
